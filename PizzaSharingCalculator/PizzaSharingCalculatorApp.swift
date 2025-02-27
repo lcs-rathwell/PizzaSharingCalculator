fc4bf44c8c255976c53f47d3ada08260ad471c42
Jack Rathwell <jrathwell@lcs.on.ca>
2025-02-27 09:41:28 -0500

    Initial Commit

---
+++ PizzaSharingCalculator/PizzaSharingCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaSharingCalculatorApp.swift
+//  PizzaSharingCalculator
+//
+//  Created by Jack William Rathwell on 2025-02-27.
+//
+
+import SwiftUI
+
+@main
+struct PizzaSharingCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
