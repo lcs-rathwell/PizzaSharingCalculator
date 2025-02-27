af75b245347bb1a2678c7f45aea1d6dffdef1127
Jack Rathwell <jrathwell@lcs.on.ca>
2025-02-27 09:47:53 -0500

    added model from playground

--- PizzaSharingCalculator/PizzaModel/Pizza.swift
+++ PizzaSharingCalculator/PizzaModel/Pizza.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+struct Pizza {
+    
+    //MARK: Stored Properties
+    
+    var numberOfPizzas: Int
+    
+    var numberOfPeople: Int
+    
+    var slicesPerPizza: Int
+    
+        //MARK: Computed Properties
+    var totalNumberOfSlices: Int {
+        
+        // find total number of slices
+        return (numberOfPizzas * slicesPerPizza)
+    }
+    var slicesPerPerson: Int{
+        // find slices per person
+        return (totalNumberOfSlices / numberOfPeople)
+    }
+    var remainingSlices: Int{
+        // find remaining slices
+        return (totalNumberOfSlices % numberOfPeople)
+    }
+}
