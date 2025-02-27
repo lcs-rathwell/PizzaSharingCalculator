ba4ff699072d46b1bb7f87080c2a9126c60563fe
Jack Rathwell <jrathwell@lcs.on.ca>
2025-02-27 09:46:41 -0500

    added project organization

---
+++ PizzaSharingCalculator/PizzaModel/Pizza.swift
@@ -0,0 +1,8 @@
+//
+//  Pizza.swift
+//  PizzaSharingCalculator
+//
+//  Created by Jack William Rathwell on 2025-02-27.
+//
+
+import Foundation
---
+++ PizzaSharingCalculator/PizzaViewModel/PizzaViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  PizzaViewModel.swift
+//  PizzaSharingCalculator
+//
+//  Created by Jack William Rathwell on 2025-02-27.
+//
+
+import SwiftUI
+
+struct PizzaViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PizzaViewModel()
+}
